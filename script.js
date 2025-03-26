e9ba4851577243594f7e5e1bdff63b49b753656f
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-03-26 12:41:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let value = document.querySelector("#text");
+let delay =document.querySelector("#delay");
+let btnsubmit = document.querySelector("#btn");
+let output = document.querySelector("#output");
+
+
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+btnsubmit.addEventListener("click",async()=>{
+// alert(delay.value);
+	let data = Number(delay.value)
+	 await wait(data);
+	output.innerHTML=value.value
+	
+	
+})
+
+// alert(typeof value)
+// alert(typeof delay)
